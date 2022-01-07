bc4fdd0978e4da285158878fe99882333c436616
bryansegovia <bsegovia@espol.edu.ec>
2022-01-06 21:29:51 -0500

    Codigo Refactorizado

--- TallerREFACTORIZADOPILAS/src/modelos/Ayudante.java
+++ TallerREFACTORIZADOPILAS/src/modelos/Ayudante.java
@@ -2,28 +2,11 @@ package modelos;
 
 import java.util.ArrayList;
 
-public class Ayudante {
-    protected Estudiante est;
+public class Ayudante extends Estudiante {
     public ArrayList<Paralelo> paralelos;
 
-    Ayudante(Estudiante e){
-    	est = e;
-    }
-    public String getMatricula() {
-        return est.getMatricula();
-    }
-
-    public void setMatricula(String matricula) {
-        est.setMatricula(matricula);
-    }
-
-    //Getters y setters se delegan en objeto estudiante para no duplicar código
-    public String getNombre() {
-        return est.getNombre();
-    }
-
-    public String getApellido() {
-        return est.getApellido();
+    public Ayudante(String matricula, String nombre, String apellido){
+        super(matricula, nombre, apellido);
     }
 
     //Los paralelos se añaden/eliminan directamente del Arraylist de paralelos
